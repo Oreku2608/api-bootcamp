06cf9dc494b121945142400816ab0ad2f79ccad2
Luis Fernandez <dev.oreku@gmail.com>
2022-11-17 19:39:34 -0600

    feat: delete task service available

--- constrollers/task-controller.js
+++ constrollers/task-controller.js
@@ -66,4 +66,19 @@ router.put("/:id", (req, res) => {
   return res.status(200).json(taskUpdated);
 });
 
+router.delete("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const task = tasks.find((task) => task.id === id);
+  if (!task)
+    return res.status(404).json({
+      error: {
+        code: "task_not_found",
+        message: "La tarea no existe",
+      },
+    });
+
+  tasks = tasks.filter((task) => task.id !== id);
+  return res.status(200).end();
+});
+
 module.exports = router;
