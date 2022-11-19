019660132563bc17666e8b67aba70d95402c12d3
Luis Fernandez <dev.oreku@gmail.com>
2022-11-19 13:17:04 -0600

    feat: dinamic port enabled

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ app.use(cors());
 
 app.use("/tasks", taskRouter);
 
-app.listen(3000, () => {
-  console.log("Running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Running on port" + PORT);
 });
