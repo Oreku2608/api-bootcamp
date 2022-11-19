081f01264690ba3fc8c279614f796c1300fc560b
Luis Fernandez <dev.oreku@gmail.com>
2022-11-19 13:14:17 -0600

    feat: cors enabled

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import taskRouter from "./constrollers/task-controller";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/tasks", taskRouter);
 
